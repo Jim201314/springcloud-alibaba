7b9c2e031c5a7a98cae9c546e3a96db5ca8fbed2
jim <250734751@qq.com>
2021-03-29 16:22:10 +0800

    lesson 5

--- springcloud-gateway/src/main/java/com/laobaozi/gateway/SpringcloudGatewayApplication.java
+++ springcloud-gateway/src/main/java/com/laobaozi/gateway/SpringcloudGatewayApplication.java
@@ -1,2 +1,14 @@
-package com.laobaozi.gateway;public class SpringcloudGatewayApplication {
+package com.laobaozi.gateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringcloudGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGatewayApplication.class, args);
+    }
 }
